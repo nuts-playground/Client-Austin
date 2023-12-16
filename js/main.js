cf8e55d7bc1701aa2d5c864d850d4d97da8e2ceb
simuseokyun <sim31059999@gmail.com>
2023-12-16 19:52:21 +0900

    feat : 다크모드

--- js/main.js
+++ js/main.js
@@ -1,17 +1,31 @@
-fetch("http://localhost:3000/User")
-  .then((res) => res.json())
-  .then((data) => {
-    let childElement;
-    const wrap = document.querySelector("#wrap");
-    for (let i = 0; i < data.length; i++) {
-      //   container.insertAdjacentHTML(
-      //     "afterbegin",
-      //     `<p>${i}번째 유저 :  ${data[i].name}</p>`
-      //   );
-      childElement = document.createElement("p");
-      childElement.className = `title${i}`;
-      childElement.innerText = `${i + 1}번째 유저 : ${data[i].name}`;
-      wrap.append(childElement);
-      //
+const loginForm = document.querySelector('#loginFormModal');
+const loginBg = document.querySelector('.loginBg');
+const loginBtn = document.querySelector('.loginBtn');
+
+loginBtn.addEventListener('click', () => {
+    loginForm.classList.add('vb');
+    loginForm.classList.remove('notVb');
+});
+loginBg.addEventListener('click', (e) => {
+    if (e.target == loginBg) {
+        loginForm.classList.add('notVb');
+        loginForm.classList.remove('vb');
     }
-  });
+});
+
+document.querySelector('.dark').addEventListener('click', (e) => {
+    document.querySelector('.light').classList.remove('hide');
+    document.querySelector('.dark').classList.add('hide');
+    document.querySelector('body').setAttribute('data-theme', 'dark');
+});
+document.querySelector('.light').addEventListener('click', (e) => {
+    document.querySelector('.dark').classList.remove('hide');
+    document.querySelector('.light').classList.add('hide');
+    document.querySelector('body').setAttribute('data-theme', 'light');
+});
+
+function sticky() {
+    const ctn = document.querySelector('.container');
+    const height = ctn.scrollTop;
+    console.log(height);
+}
