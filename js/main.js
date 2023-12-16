8caa3da9fdf301c9d4728f6442a62d51fa48f1cc
simuseokyun <sim31059999@gmail.com>
2023-12-16 14:58:22 +0900

    feat : 메인페이지 제작

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-document.getElementById("getPlaylist").addEventListener("click", () => {
-  fetch("/js/data.json")
-    .then((res) => res.json())
-    .then((data) => {
-      let playlist = data.PLAYLIST;
-    });
-});
+fetch("http://localhost:3000/User")
+  .then((res) => res.json())
+  .then((data) => {
+    let childElement;
+    const wrap = document.querySelector("#wrap");
+    for (let i = 0; i < data.length; i++) {
+      //   container.insertAdjacentHTML(
+      //     "afterbegin",
+      //     `<p>${i}번째 유저 :  ${data[i].name}</p>`
+      //   );
+      childElement = document.createElement("p");
+      childElement.className = `title${i}`;
+      childElement.innerText = `${i + 1}번째 유저 : ${data[i].name}`;
+      wrap.append(childElement);
+    }
+  });
