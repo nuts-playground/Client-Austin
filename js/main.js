1922e06207fb6dd2c107aade7b71760164270201
simuseokyun <sim31059999@gmail.com>
2023-12-18 18:41:13 +0900

    feat : 로그인 alert

--- js/main.js
+++ js/main.js
@@ -2,27 +2,54 @@ const loginForm = document.querySelector('#loginFormModal');
 const loginBg = document.querySelector('.loginBg');
 const loginBtn = document.querySelector('.loginBtn');
 
-loginBtn.addEventListener('click', () => {
-    loginForm.classList.add('vb');
-    loginForm.classList.remove('notVb');
-});
+loginBtn.addEventListener('click', toggleFormVisibility);
 loginBg.addEventListener('click', (e) => {
     if (e.target === loginBg) {
-        loginForm.classList.add('notVb');
-        loginForm.classList.remove('vb');
+        toggleFormVisibility();
     }
 });
 
-document.querySelector('.dark').addEventListener('click', (e) => {
-    document.querySelector('.light').classList.remove('hide');
-    document.querySelector('.dark').classList.add('hide');
-    document.querySelector('body').setAttribute('data-theme', 'dark');
+document.querySelector('.dark').addEventListener('click', toggleTheme);
+document.querySelector('.light').addEventListener('click', toggleTheme);
+
+document.querySelector('.clickLogin').addEventListener('click', (e) => {
+    fetch('/js/db.json').then((res) =>
+        res.json().then((data) => {
+            const User = data.USER;
+            const LoginId = document.querySelector('#username').value;
+            const LoginPw = document.querySelector('#password').value;
+            const CheckUser = User.find((user) => user.ID === LoginId && user.PW === LoginPw);
+
+            if (CheckUser) {
+                alert(`환영합니다 ${CheckUser.ID}님`);
+            } else {
+                alert('아이디 또는 비밀번호가 일치하지 않습니다.');
+                document.querySelector('.loginForm').submit = function () {
+                    return false;
+                };
+            }
+        })
+    );
 });
-document.querySelector('.light').addEventListener('click', (e) => {
-    document.querySelector('.dark').classList.remove('hide');
-    document.querySelector('.light').classList.add('hide');
-    document.querySelector('body').setAttribute('data-theme', 'light');
-});
+// 함수영역
+function toggleTheme() {
+    const body = document.querySelector('body');
+    const darkButton = document.querySelector('.dark');
+    const lightButton = document.querySelector('.light');
+    if (body.getAttribute('data-theme') === 'dark') {
+        body.setAttribute('data-theme', 'light');
+        darkButton.classList.remove('hide');
+        lightButton.classList.add('hide');
+    } else {
+        body.setAttribute('data-theme', 'dark');
+        lightButton.classList.remove('hide');
+        darkButton.classList.add('hide');
+    }
+}
+function toggleFormVisibility() {
+    loginForm.classList.toggle('vb');
+    loginForm.classList.toggle('notVb');
+}
 
 function sticky() {
     const ctn = document.querySelector('.container');
