659cb8a0ec99f3d938c32fbaf65a53ff0505b048
simuseokyun <sim31059999@gmail.com>
2023-12-21 22:09:53 +0900

    test : 그냥 공부

--- js/test.js
+++ js/test.js
@@ -1,3 +1,4 @@
+//클로저
 function makeFunc() {
     const name = 'Mozilla';
     function displayName() {
@@ -8,3 +9,27 @@ function makeFunc() {
 
 const myFunc = makeFunc();
 myFunc();
+
+const counter = (function () {
+    let privateCounter = 0;
+    function changeBy(val) {
+        privateCounter += val;
+    }
+
+    return {
+        increment() {
+            changeBy(1);
+        },
+
+        decrement() {
+            changeBy(-1);
+        },
+
+        value() {
+            return privateCounter;
+        },
+    };
+})();
+console.log(counter.value());
+
+//페이지 네이션 구현해보기
