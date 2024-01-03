73911b1b262b9c652202870bafb9bd4488654e55
simuseokyun <sim31059999@gmail.com>
2024-01-03 21:05:39 +0900

    refactor

--- study/lotto/main.js
+++ study/lotto/main.js
@@ -17,10 +17,11 @@ const shuffleArr = (arr) => {
     }
     return shuffle;
 };
-const setTimeShow = (selector, index, delay) => {
+const setTimeShow = (selector, index, delay, i) => {
     setTimeout(() => {
-        const temp = `<div class="ball">${index}</div>`;
+        const temp = `<div class="ball" data-id="${i}">${index}</div>`;
         selector.insertAdjacentHTML('beforeend', temp);
+        colorize(index, i);
     }, delay);
 };
 
@@ -28,12 +29,28 @@ const showNumbers = (numbers) => () => {
     const selectNum = numbers.slice(0, 7);
     const bonus = selectNum[selectNum.length - 1];
     for (let i = 0; i < selectNum.length - 1; i++) {
-        setTimeShow($('#result'), selectNum[i], (i + 1) * 1000);
+        setTimeShow($('#result'), selectNum[i], (i + 1) * 1000, i + 1);
     }
-    setTimeShow($('#bonus'), selectNum[selectNum.length - 1], 7000);
+    setTimeShow($('#bonus'), bonus, 7000, 7);
 };
 
 let numbers = generateNum();
 let shuffleNumbers = shuffleArr(numbers);
 
 $('button').addEventListener('click', showNumbers(shuffleNumbers));
+
+function colorize(number, index) {
+    if (number < 10) {
+        document.querySelectorAll('.ball')[index - 1].style.backgroundColor = 'red';
+        document.querySelectorAll('.ball')[index - 1].style.color = 'white';
+    } else if (number < 20) {
+        document.querySelectorAll('.ball')[index - 1].style.backgroundColor = 'orange';
+    } else if (number < 30) {
+        document.querySelectorAll('.ball')[index - 1].style.backgroundColor = 'green';
+    } else if (number < 40) {
+        document.querySelectorAll('.ball')[index - 1].style.backgroundColor = 'yellow';
+    } else {
+        document.querySelectorAll('.ball')[index - 1].style.backgroundColor = 'black';
+        document.querySelectorAll('.ball')[index - 1].style.color = 'white';
+    }
+}
